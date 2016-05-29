e9223395c2346349391a1b20154edc327eeff042
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 11:42:48 -0400

    major module controller framework

---
+++ src/mathemaster/Sciences.java
@@ -0,0 +1,13 @@
+/*
+ * This is the sciences module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
+ */
+package mathemaster;
+
+public class Sciences {
+    
+    public void sciencesController() {
+        System.out.println("sciences reached - module under construction");
+    }
+}
