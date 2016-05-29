705b54005969d3f3cf918338e266b91de26b2de7
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 13:04:42 -0400

    advanced mathematics controller framework in progress, need to create trig, calc, and statistics modules

--- src/mathemaster/AdvancedMathematics.java
+++ src/mathemaster/AdvancedMathematics.java
@@ -1,17 +1,47 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is the advanced math module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
  */
 package mathemaster;
 
-/**
- *
- * @author Kelly
- */
+import java.util.*;
+
 public class AdvancedMathematics {
     
     public void advancedMathematicsController() {
-        System.out.println("advanced mathematics reached - module under construction");
+        int userSelection = 0;
+        
+        System.out.println( "Welcome to the Advanced mathematics module!" + "\n" +
+                            "The current installed modules are:"
+        ); // This is the header and instruction block outprinted to the user.
+    //^^^^^^^^^^^^^^^^^^^^^^^^^Welcome message^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+        System.out.print(   "#1: Geometry" + "\n" +
+                            "#2: Algebra" + "\n" +
+                            "#3: Trigonometry" + "\n" +
+                            "#4: Calculus" + "\n" +
+                            "#5: Statistics" + "\n" +
+        ""); // This is the list of installed modules. 
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^Module list^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+        ValidateInput runValidation = new ValidateInput();
+        userSelection = runValidation.validateInput();
+    //^^^^^^^^^^^^^^^^^^^^^Get valid user selection block^^^^^^^^^^^^^^^^^^^^^^^
+        
+        switch(userSelection) {
+            case 1:
+                Geometry runGeometry = new Geometry();
+                runGeometry.geometry();
+                break;
+            case 2:
+                Algebra runAlgebra = new Algebra();
+                runAlgebra.algebra();
+                break;
+            
+        } // This block runs the appropriate module, based on the user selection.
+    //^^^^^^^^^^^^^^^^^^^^^^^^user selection and evaluation^^^^^^^^^^^^^^^^^^^^^
     }
+    
+    
 }
