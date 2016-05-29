3ae824dd5bee40d46683e6b762a7c392a4a05950
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 11:42:42 -0400

    major module controller framework

---
+++ src/mathemaster/Physics.java
@@ -0,0 +1,13 @@
+/*
+ * This is the Physics module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
+ */
+package mathemaster;
+
+public class Physics {
+    
+    public void physicsController() {
+        System.out.println("physics reached - module under construction");
+    }
+}
