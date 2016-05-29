f00321d0656e3094b2f7f8aacd5d868251d817bc
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 11:42:39 -0400

    major module controller framework

---
+++ src/mathemaster/AdvancedMathematics.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathemaster;
+
+/**
+ *
+ * @author Kelly
+ */
+public class AdvancedMathematics {
+    
+    public void advancedMathematicsController() {
+        System.out.println("advanced mathematics reached - module under construction");
+    }
+}
