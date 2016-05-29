f44ffecc08a67b8d5a31cdc63396d0323578aab8
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 11:42:45 -0400

    major module controller framework

---
+++ src/mathemaster/Chemistry.java
@@ -0,0 +1,13 @@
+/*
+ * This is the chemistry module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
+ */
+package mathemaster;
+
+public class Chemistry {
+    
+    public void chemistryController() {
+        System.out.println("chemistry reached - module under construction");
+    }
+}
