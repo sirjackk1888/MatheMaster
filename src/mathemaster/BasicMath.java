35dfc13fe5d0f138531ee3f86dbad2e2c80e8090
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 11:24:19 -0400

    initial framework for the basic math module

---
+++ src/mathemaster/BasicMath.java
@@ -0,0 +1,13 @@
+/*
+ * This is the basic math module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
+ */
+package mathemaster;
+
+public class BasicMath {
+    
+    public void basicMath() {
+        System.out.println("basic math reached - module under construction");
+    }
+}
