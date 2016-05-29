959cc16a6abf82a1bb44d32633b0708e9e02df92
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 13:05:23 -0400

    Economics framework empty

---
+++ src/mathemaster/Economics.java
@@ -0,0 +1,36 @@
+/*
+ * This is the Economics module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
+ */
+package mathemaster;
+
+import java.util.*;
+
+public class Economics {
+    
+    public void economicsController() {
+        int userSelection = 0;
+        
+        System.out.println( "Welcome to the Economics module!" + "\n" +
+                            "The current installed modules are:"
+        ); // This is the header and instruction block outprinted to the user.
+    //^^^^^^^^^^^^^^^^^^^^^^^^^Welcome message^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+        System.out.print(   "#1: " + "\n" +
+        ""); // This is the list of installed modules. 
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^Module list^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+        ValidateInput runValidation = new ValidateInput();
+        userSelection = runValidation.validateInput();
+    //^^^^^^^^^^^^^^^^^^^^^Get valid user selection block^^^^^^^^^^^^^^^^^^^^^^^
+        
+        switch(userSelection) {
+            case 1:
+               
+                break;
+            
+        } // This block runs the appropriate module, based on the user selection.
+    //^^^^^^^^^^^^^^^^^^^^^^^^user selection and evaluation^^^^^^^^^^^^^^^^^^^^^
+    }
+}
