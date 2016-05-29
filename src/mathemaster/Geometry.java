293701b259b2860f0e2875c3b12af89be3950bac
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 11:24:30 -0400

    initial framework for the geometry module

---
+++ src/mathemaster/Geometry.java
@@ -0,0 +1,13 @@
+/*
+ * This is the geometry module of the MatheMaster calculator
+ * 
+ * coded by: Kelly B Ward
+ */
+package mathemaster;
+
+public class Geometry {
+    
+    public void geometry() {
+        System.out.println("geometry reached - module under construction");
+    }
+}
