b3e67f25fc5dc7cd6123faeef3f38ebca959d572
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-29 15:48:41 -0400

    Completed sciences framework

--- src/mathemaster/Sciences.java
+++ src/mathemaster/Sciences.java
@@ -5,9 +5,34 @@
  */
 package mathemaster;
 
+import java.util.*;
+
 public class Sciences {
     
+    ValidateInput runValidation = new ValidateInput();
+    
     public void sciencesController() {
-        System.out.println("sciences reached - module under construction");
+        int userSelection = 0;
+        
+        System.out.println( "Welcome to the Sciences module!" + "\n" +
+                            "The current installed modules are:"
+        ); // This is the header and instruction block outprinted to the user.
+    //^^^^^^^^^^^^^^^^^^^^^^^^^Welcome message^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+        System.out.print(   "#1: Temperature Conversion" + "\n" +
+        ""); // This is the list of installed modules. 
+    //^^^^^^^^^^^^^^^^^^^^^^^^^^^Module list^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+        userSelection = runValidation.getUserSelection();
+    //^^^^^^^^^^^^^^^^^^^^^Get valid user selection block^^^^^^^^^^^^^^^^^^^^^^^
+        
+        switch(userSelection) {
+            case 1:
+                TemperatureConversion runTemperatureConversion = new TemperatureConversion();
+                runTemperatureConversion.temperatureConversion();
+                break;
+            
+        } // This block runs the appropriate module, based on the user selection.
+    //^^^^^^^^^^^^^^^^^^^^^^^^user selection and evaluation^^^^^^^^^^^^^^^^^^^^^
     }
 }
